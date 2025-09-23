996b2caaafe2a67f4923ddc40cf72398f84c1002
Denilson Silva <121098375+denilson-silvestre@users.noreply.github.com>
2025-09-23 11:15:04 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,9 +15,4 @@ exec odoo \
     --db_port="${ODOO_DATABASE_PORT}" \
     --db_user="${ODOO_DATABASE_USER}" \
     --db_password="${ODOO_DATABASE_PASSWORD}" \
-    --database="${ODOO_DATABASE_NAME}" \
-    --smtp="${ODOO_SMTP_HOST}" \
-    --smtp-port="${ODOO_SMTP_PORT_NUMBER}" \
-    --smtp-user="${ODOO_SMTP_USER}" \
-    --smtp-password="${ODOO_SMTP_PASSWORD}" \
-    --email-from="${ODOO_EMAIL_FROM}" 2>&1
+    --database="${ODOO_DATABASE_NAME}" 2>&1
