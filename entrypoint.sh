f70b8310b07dc8be6106ce5588354c917fc5922c
Denilson Silva <121098375+denilson-silvestre@users.noreply.github.com>
2025-09-26 09:43:34 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,15 +8,6 @@ while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 1; done
 
 echo Database is now available
 
-# MIGRAÇÃO: uma passada e sai
-if [ "${ODOO_RUN_MIGRATION:-0}" = "1" ]; then
-  echo "Running DB migration (Odoo -u base,web,auth_totp)..."
-  odoo -c /etc/odoo/odoo.conf \
-       -d "${ODOO_DATABASE_NAME}" \
-       -u base,web,auth_totp \
-       --stop-after-init
-fi
-
 exec odoo \
     --http-port="${PORT}" \
     --proxy-mode \
