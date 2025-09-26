adbc8c0e2e6fee8816d681963e989732763a29f2
Denilson Silva <121098375+denilson-silvestre@users.noreply.github.com>
2025-09-26 09:36:25 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,12 @@ while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 1; done
 
 echo Database is now available
 
+# 1) Migração de schema (uma passada)
+if [ "${ODOO_RUN_MIGRATION:-1}" = "1" ]; then
+  echo "Running one-shot module upgrade (-u all)..."
+  odoo -c /etc/odoo/odoo.conf -u all --stop-after-init
+fi
+
 exec odoo \
     --http-port="${PORT}" \
     --proxy-mode \
