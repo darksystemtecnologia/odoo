3b0ed900168ddf4b7fe5636558aeea0351eb3114
Denilson Silva <121098375+denilson-silvestre@users.noreply.github.com>
2025-09-26 09:26:55 -0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,32 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/sh
+
 set -e
 
-echo "Waiting for database..."
-while ! nc -z "${ODOO_DATABASE_HOST}" "${ODOO_DATABASE_PORT}" 2>&1; do sleep 1; done
-echo "Database is now available"
+echo Waiting for database...
 
-: "${ODOO_ADMIN_PASSWD:?Defina a env ODOO_ADMIN_PASSWD no Railway com uma senha forte}"
+while ! nc -z ${ODOO_DATABASE_HOST} ${ODOO_DATABASE_PORT} 2>&1; do sleep 1; done; 
 
-# Gera o /etc/odoo/odoo.conf a partir das envs do Railway
-mkdir -p /etc/odoo
-cat >/etc/odoo/odoo.conf <<EOF
-[options]
-proxy_mode = True
-http_port = ${PORT}
+echo Database is now available
 
-db_host = ${ODOO_DATABASE_HOST}
-db_port = ${ODOO_DATABASE_PORT}
-db_user = ${ODOO_DATABASE_USER}
-db_password = ${ODOO_DATABASE_PASSWORD}
-db_name = ${ODOO_DATABASE_NAME}
-db_filter = ^${ODOO_DATABASE_NAME}$
-
-list_db = False
-admin_passwd = ${ODOO_ADMIN_PASSWD}
-
-addons_path = /usr/lib/python3/dist-packages/odoo/addons,/var/lib/odoo/addons
-EOF
-
-# Sobe o Odoo lendo essa config
-exec odoo -c /etc/odoo/odoo.conf 2>&1
-
+exec odoo \
+    --http-port="${PORT}" \
+    --proxy-mode \
+    --db_host="${ODOO_DATABASE_HOST}" \
+    --db_port="${ODOO_DATABASE_PORT}" \
+    --db_user="${ODOO_DATABASE_USER}" \
+    --db_password="${ODOO_DATABASE_PASSWORD}" \
+    --db-filter="^${ODOO_DATABASE_NAME}$" \
+    --database="${ODOO_DATABASE_NAME}" 2>&1
